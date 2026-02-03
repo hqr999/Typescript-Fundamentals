9088b6c514c7cf6c18fe0c497ccdadcccdbd1229
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2026-02-03 15:52:37 -0300

    Typescript types

---
+++ typescript/src/typeAnotations.ts
@@ -0,0 +1,8 @@
+let awesome: string = "Shake and Blake";
+awesome = "algum";
+awesome = awesome.toUpperCase();
+console.log(awesome);
+
+let amount: number = 20;
+amount = 12 - 1;
+//amount = pants - errado
---
+++ typescript/src/typeChallenge.ts
@@ -0,0 +1,8 @@
+let saudacao: string = "Bom Dia";
+saudacao = saudacao.toLowerCase();
+
+let age: number = 25;
+age = age + 5;
+
+let opLogica: boolean = age >= 18;
+console.log(opLogica);
